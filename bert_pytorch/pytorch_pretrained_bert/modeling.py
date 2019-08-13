4226b5eb046f8c983ebdc61851f4377c3172dace
waby <wabyking@163.com>
2019-08-13 20:12:04 +0200

    remove capsule in the pytorch project

--- bert_pytorch/pytorch_pretrained_bert/modeling.py
+++ bert_pytorch/pytorch_pretrained_bert/modeling.py
@@ -36,7 +36,7 @@ from torch.nn import CrossEntropyLoss
 
 from .file_utils import cached_path
 
-from capsule_net import capsnet
+
 
 logger = logging.getLogger(__name__)
 
@@ -986,109 +986,6 @@ class BertForSequenceClassification(BertPreTrainedModel):
             return logits
 
 
-class BertForSequenceCapsuleClassification_meta(BertPreTrainedModel):
-    """BERT model for classification with capsule.
-        using MAML to capsule weights.
-    """
-    def __init__(self, config, num_labels, output_atoms=30, lr_a=0.00005):
-        super(BertForSequenceCapsuleClassification_meta, self).__init__(config)
-        self.num_labels = num_labels
-        self.bert = BertModel(config)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        # ---
-
-        self.capsule_layer = capsnet.CapsuleLayer(1, output_dim=num_labels, input_atoms=768, output_atoms=output_atoms, num_routing=1, leaky=True)
-        self.capsule_classification = capsnet.CapsuleClassification()
-
-        self.lr_a = lr_a
-
-        self.apply(self.init_bert_weights)
-
-    def forward_pass(self, fast_weights, input_ids, token_type_ids=None, attention_mask=None, labels=None):
-
-        onehot_labels = torch.zeros(labels.shape[0], self.num_labels).to(labels.device.type).scatter_(dim=1, index=labels.unsqueeze(1), value=1.)
-
-        _, pooled_output = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
-        pooled_output = self.dropout(pooled_output)
-        pooled_output = pooled_output.unsqueeze(1)  # [b, 1, hid]
-
-        capsule_layer_output = self.capsule_layer(pooled_output, fast_weights)
-        logits, probs = self.capsule_classification(capsule_layer_output)
-        loss = self.capsule_layer._margin_loss(onehot_labels, logits)
-        return loss
-
-    def forward(self, train_dataloader, test_data, device):
-
-        fast_weights = OrderedDict((name, param) for (name, param) in self.capsule_layer.named_parameters())
-        test_input_ids = test_data[0].to(device)
-        test_token_type_ids = test_data[1].to(device)
-        test_attention_mask = test_data[2].to(device)
-        test_labels = test_data[3].to(device)
-
-        for _ in range(3):  # epoch
-            for step, batch in enumerate(train_dataloader):
-                batch = tuple(t.to(device) for t in batch)
-                input_ids, input_mask, segment_ids, label_ids = batch
-
-                onehot_labels = torch.zeros(label_ids.shape[0], self.num_labels).to(label_ids.device.type).scatter_(dim=1, index=label_ids.unsqueeze(1), value=1.)
-
-                _, pooled_output = self.bert(input_ids, segment_ids, input_mask, output_all_encoded_layers=False)
-                pooled_output = self.dropout(pooled_output)
-                pooled_output = pooled_output.unsqueeze(1)  # [b, 1, hid]
-
-                if step == 0:
-                    capsule_layer_output = self.capsule_layer(pooled_output)
-                    logits, probs = self.capsule_classification(capsule_layer_output)
-                    loss = self.capsule_layer._margin_loss(onehot_labels, logits)
-                    grads = torch.autograd.grad(loss, self.capsule_layer.parameters(), create_graph=True)
-                else:
-                    capsule_layer_output = self.capsule_layer(pooled_output, fast_weights)
-                    logits, probs = self.capsule_classification(capsule_layer_output)
-                    loss = self.capsule_layer._margin_loss(onehot_labels, logits)
-                    grads = torch.autograd.grad(loss, fast_weights.values(), create_graph=True)
-
-                fast_weights = OrderedDict((name, param - self.lr_a * grad) for ((name, param), grad) in zip(fast_weights.items(), grads))
-
-        meta_loss = self.forward_pass(fast_weights, test_input_ids, test_token_type_ids, test_attention_mask, test_labels)
-        # meta_grads = torch.autograd.grad(meta_loss, self.parameters())
-        # meta_grads = {name: g for ((name, _), g) in zip(self.named_parameters(), meta_grads)}
-
-        return meta_loss   # meta_grads
-
-
-class BertForSequenceCapsuleClassification(BertPreTrainedModel):
-    """BERT model for classification with capsule.
-    """
-    def __init__(self, config, num_labels, output_atoms=30, meta_weights=None):
-        super(BertForSequenceCapsuleClassification, self).__init__(config)
-        self.num_labels = num_labels
-        self.bert = BertModel(config)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        # ---
-        if meta_weights is None:
-            self.capsule_layer = capsnet.CapsuleLayer(1, output_dim=num_labels, input_atoms=768, output_atoms=output_atoms, num_routing=1, leaky=True)
-        else:
-            self.capsule_layer = capsnet.CapsuleLayer(1, output_dim=num_labels, input_atoms=768,
-                                                              output_atoms=output_atoms, paras=meta_weights, num_routing=1, leaky=True)
-        self.capsule_classification = capsnet.CapsuleClassification()
-
-        self.apply(self.init_bert_weights)
-
-    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None):
-        _, pooled_output = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
-        pooled_output = self.dropout(pooled_output)
-        pooled_output = pooled_output.unsqueeze(1)  # [b, 1, hid]
-
-
-        capsule_layer_output = self.capsule_layer(pooled_output)
-        logits, probs = self.capsule_classification(capsule_layer_output)
-
-        if labels is not None:
-            onehot_labels = torch.zeros(labels.shape[0], self.num_labels).to(labels.device.type).scatter_(dim=1, index=labels.unsqueeze(1), value=1.)
-            loss = self.capsule_layer._margin_loss(onehot_labels, logits)
-            return loss
-        else:
-            return probs
 
 
 class BertForMultipleChoice(BertPreTrainedModel):
