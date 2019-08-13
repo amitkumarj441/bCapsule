2d8c79f88911f2d2428b7a9d0f30a79e0bc55c46
Wang Benyou <wang@login.dei.unipd.it>
2019-08-13 13:54:40 +0200

    shabi amit

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 export CUDA_VISIBLE_DEVICES=0
-export PYTHONPATH=$PYTHONPATH:/home/amit/bCapsule
+export PYTHONPATH=$PYTHONPATH:./bCapsule
 
-python /home/amit/bCapsule/bert_pytorch/examples/run_classifier.py --data_dir=/home/amit/bCapsule/data --task_name=bCapsule \
+python ./bert_pytorch/examples/run_classifier.py --data_dir=/home/amit/bCapsule/data --task_name=bCapsule \
     --do_train \
 	--do_eval \
-    --bert_model=/home/amit/bCapsule/pre_trained \
+    --bert_model=./pre_trained \
     --max_seq_length=100 \
 	--Nways=10 \
 	--Kshot=5 \
@@ -13,4 +13,4 @@ python /home/amit/bCapsule/bert_pytorch/examples/run_classifier.py --data_dir=/h
     --lr_a=5e-4 \
 	--learning_rate=5e-5 \
     --test_train_epochs=8 \
-    --output_dir=/home/amit/bCapsule/output
+    --output_dir=./output
